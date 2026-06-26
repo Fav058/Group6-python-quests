203cf3cbc853a435017c13d8818389c8d149d554
fave058 <f.kebei@alustudent.com>
2026-06-26 11:45:15 -1100

    Complete Quest 22

---
+++ Desktop/group6-python-quests/quests/quest_22_personalized_scroll.py
@@ -0,0 +1,7 @@
+def personalized_greeting(name, quest):
+    print(f"Hello {name}! Good luck with your quest: {quest}.")
+
+name = input("Enter your name: ")
+quest = input("Enter your quest: ")
+
+personalized_greeting(name, quest)
