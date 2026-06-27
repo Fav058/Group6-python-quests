beba1438d56df182d22a8a28fe6d71b6a528859d
jlouange <j.mushumba@alustudent.com>
2026-06-27 20:37:40 +0200

    Added new error handling logic

--- quests/quest_13_maze_of_many_choices.py
+++ quests/quest_13_maze_of_many_choices.py
@@ -1,11 +1,15 @@
 while True:
 
   try:
+    
     score = int(input("Enter the score(0-100): "))
-    break
+    if score in range(0,101):
+      break
+    else:
+      raise ValueError
   
   except ValueError:
-    print("Please enter a valid number between")
+    print("Please enter a valid number between(0-100)")
 
 if score in range(90,101):
   print("A")
@@ -15,6 +19,6 @@ elif score in range(80,90):
 
 elif score in range(70,80):
   print("C")
-  
+
 else:
   print("Needs Improvement")
